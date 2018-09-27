89bb771f4794efec8f8ae3135555e9ba4c2313d3
jlk98 <43638616+jlk98@users.noreply.github.com>
2018-09-27 14:25:45 +0200

    Update Fahrzeug.cs

--- Fahrzeugverwaltung/Fahrzeug.cs
+++ Fahrzeugverwaltung/Fahrzeug.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Fahrzeugverwaltung
 {
-    class Fahrzeug
+    abstract class Fahrzeug
     {
+        public string Herseller {get; set;}
+        public string Modell { get; set; }
+        public string Kennzeichen { get; set; }
+        public int jahrDerErstzulassung { get; set; }
+        public double Anschaffungspreis { get; set; }
+        public bool steuerBezahlt { get; set; }
+
+        public Fahrzeug(string hersteller, string modell, string kennzeichen, int erstzulassung, double preis, bool steuer)
+        {
+            this.Herseller = hersteller;
+            this.jahrDerErstzulassung = erstzulassung;
+            this.Modell = modell;
+            this.steuerBezahlt = steuer;
+            this.Kennzeichen = kennzeichen;
+            this.Anschaffungspreis = preis;
+        }
+
+        public virtual void berechneSteuer()
+        {
+
+        }
+
+
+        
     }
 }
