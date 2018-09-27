751e73fc7714333674773d47c07a6c4271b8e9b2
jlk98 <43638616+jlk98@users.noreply.github.com>
2018-09-27 14:34:30 +0200

    Update TestClass.cs

--- Fahrzeugverwaltung/TestClass.cs
+++ Fahrzeugverwaltung/TestClass.cs
@@ -8,6 +8,15 @@ namespace Fahrzeugverwaltung
 {
     class TestClass
     {
-        
+        public void Test()
+        {
+            Fahrzeug A3 = new PKW("audi", "a3", "Dfjsg", 34, 23354, true, 34, 234, 1);
+            Fahrzeug Scania12 = new LKW("Scania", "12", "ASFh", 1241, 342, true, 6, 21);
+            Fahrzeug Kawa23 = new Motorrad("Kawasaki", "K12", "asdf", 324, 123, true, 34);
+
+            A3.berechneSteuer();
+            Scania12.berechneSteuer();
+            Kawa23.berechneSteuer();
+        }
     }
 }
