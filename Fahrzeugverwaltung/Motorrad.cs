b66833795477237533a91c36ece654f3639d02f8
jlk98 <43638616+jlk98@users.noreply.github.com>
2018-09-27 14:26:38 +0200

    Update Motorrad.cs

--- Fahrzeugverwaltung/Motorrad.cs
+++ Fahrzeugverwaltung/Motorrad.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Fahrzeugverwaltung
 {
-    class Motorrad
+    class Motorrad : Fahrzeug
     {
+        private int Hubraum { get; set; }
+
+        public Motorrad(string hersteller, string modell, string kennzeichen, int erstzulassung, double preis, bool steuer, int hubraum)
+            : base (hersteller, modell, kennzeichen, erstzulassung, preis, steuer)
+        {
+            this.Hubraum = hubraum;
+        }
+
+        public override void berechneSteuer()
+        {
+            double Steuern = (Hubraum * 99) / 100 * 20;
+            Console.WriteLine("Die Steuern für das Motorrad " + base.Kennzeichen + " betragen " + Steuern + " Euro.");
+        }
     }
 }
