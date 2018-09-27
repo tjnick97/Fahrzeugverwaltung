e9bde16a7612fa6153f14938c2d478f8765d4a36
jlk98 <43638616+jlk98@users.noreply.github.com>
2018-09-27 14:27:01 +0200

    Update PKW.cs

--- Fahrzeugverwaltung/PKW.cs
+++ Fahrzeugverwaltung/PKW.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Fahrzeugverwaltung
 {
-    class PKW
+    class PKW : Fahrzeug
     {
+        private int Hubraum { get; set; }
+        private int Leistung { get; set; }
+        private int Schadstoffklasse { get; set; }
+
+        public PKW(string hersteller, string modell, string kennzeichen, int erstzulassung, double preis, bool steuer, int hubraum, int leistung, int schadstoffklasse) 
+            : base(hersteller,  modell, kennzeichen, erstzulassung, preis, steuer)
+        {
+            this.Hubraum = hubraum;
+            this.Leistung = leistung;
+            this.Schadstoffklasse = schadstoffklasse;
+        }
+
+        public override void berechneSteuer()
+        {
+            double Steuern = (Hubraum + 99) / 100 * 10 * (Schadstoffklasse + 1);
+            Console.WriteLine("Die Steuern für den PKW " + base.Kennzeichen + " beträgt " + Steuern + " Euro.");
+        }
     }
 }
