1bebad16520714d68b1e3d3d6959fe343b4f07a2
jlk98 <43638616+jlk98@users.noreply.github.com>
2018-09-27 13:15:22 +0200

    Update Program.cs

--- Fahrzeugverwaltung/Program.cs
+++ Fahrzeugverwaltung/Program.cs
@@ -17,6 +17,7 @@ namespace Fahrzeugverwaltung
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            System.help;
         }
     }
 }
